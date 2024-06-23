d5acbda70f681fec484c3fdfcfa906bdb81aabc5
emeraldhycient <igwezehycient86@gmail.com>
2024-06-23 20:28:43 +0200

    fix: prisma fail

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -40,6 +40,11 @@ const statusPill = (status: string) => {
 
 const InvoicesPage: React.FC = () => {
     const [invoices, setInvoices] = useState<Invoice[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [filter, setFilter] = useState<string>('All');
+    const [sortBy, setSortBy] = useState<string>('Date');
+    const [currentPage, setCurrentPage] = useState(1);
+    const invoicesPerPage = 5;
 
     useEffect(() => {
         const fetchInvoices = async () => {
@@ -51,11 +56,39 @@ const InvoicesPage: React.FC = () => {
             } catch (error) {
                 console.error('Error fetching invoices:', error);
                 setInvoices(dummyInvoices); // Use dummy data on error
+            } finally {
+                setLoading(false);
             }
         };
 
         fetchInvoices();
     }, []);
+
+    const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setFilter(event.target.value);
+    };
+
+    const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSortBy(event.target.value);
+    };
+
+    const handlePageChange = (newPage: number) => {
+        setCurrentPage(newPage);
+    };
+
+    const filteredInvoices = invoices.filter((invoice) => filter === 'All' || invoice.status === filter);
+    const sortedInvoices = filteredInvoices.sort((a, b) => {
+        if (sortBy === 'Amount') {
+            return b.totalAmount - a.totalAmount;
+        }
+        // Assuming 'Date' is the default sorting criteria
+        return b.id - a.id;
+    });
+
+    const paginatedInvoices = sortedInvoices.slice(
+        (currentPage - 1) * invoicesPerPage,
+        currentPage * invoicesPerPage
+    );
 
     return (
         <div className="bg-dashboard-bg min-h-screen text-dashboard-text">
@@ -71,30 +104,89 @@ const InvoicesPage: React.FC = () => {
                     </Button>
                 }
             />
-            <ul className="space-y-4 p-8 md:px-24 md:py-16">
-                {invoices.map((invoice) => (
-                    <Link key={invoice.id} href={`/dashboard/invoices/${invoice.id}`}>
-                        <li className="bg-cardBg p-4 mb-3 rounded-lg shadow-lg flex justify-between transform transition-transform hover:scale-105 hover:shadow-xl cursor-pointer">
+            <div className="flex justify-between items-center p-8 md:px-24 md:py-4">
+                <div className="flex items-center space-x-4">
+                    <div className="flex items-center space-x-4">
+                        <label htmlFor="filter" className="text-sm font-semibold text-gray-400">Filter by Status:</label>
+                        <select
+                            id="filter"
+                            value={filter}
+                            onChange={handleFilterChange}
+                            className="bg-cardBg text-gray-400 px-2 py-1 rounded text-sm"
+                        >
+                            <option value="All">All</option>
+                            <option value="Paid">Paid</option>
+                            <option value="Pending">Pending</option>
+                            <option value="Overdue">Overdue</option>
+                        </select>
+                    </div>
+                    <div className="flex items-center space-x-4">
+                        <label htmlFor="sort" className="text-sm font-semibold text-gray-400">Sort by:</label>
+                        <select
+                            id="sort"
+                            value={sortBy}
+                            onChange={handleSortChange}
+                            className="bg-cardBg text-gray-400 px-2 py-1 rounded text-sm"
+                        >
+                            <option value="Date">Date</option>
+                            <option value="Amount">Amount</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <ul className="space-y-4 p-8 md:px-24 md:py-4">
+                {loading
+                    ? [...Array(invoicesPerPage)].map((_, index) => (
+                        <li key={index} className="bg-cardBg p-4 mb-3 rounded-lg shadow-lg flex justify-between animate-pulse">
                             <div className="flex items-center space-x-4">
-                                <div className="text-xl flex-shrink-0">
-                                    {statusIcon(invoice.status)}
-                                </div>
+                                <div className="bg-gray-700 rounded-full h-6 w-6"></div>
                                 <div className="flex flex-col items-start">
-                                    <span className="block text-sm font-semibold">Invoice #{invoice.id}</span>
-                                    <span className="block text-xs text-gray-400">Client: {invoice.client.name}</span>
-                                    <span className="block text-xs text-gray-400">Amount: ${invoice.totalAmount.toFixed(2)}</span>
+                                    <div className="h-4 bg-gray-700 rounded w-32 mb-2"></div>
+                                    <div className="h-3 bg-gray-700 rounded w-20"></div>
+                                    <div className="h-3 bg-gray-700 rounded w-20"></div>
                                 </div>
                             </div>
                             <div className="flex items-center space-x-4">
-                                {statusPill(invoice.status)}
-                                <Link href={`/invoices/${invoice.id}/download`} className="flex items-center text-green-400 hover:text-green-500 transition-all duration-300 text-xs">
-                                    <FaFileDownload size={16} className="mr-1" />
-                                </Link>
+                                <div className="h-4 bg-gray-700 rounded w-16"></div>
                             </div>
                         </li>
-                    </Link>
-                ))}
+                    ))
+                    : paginatedInvoices.map((invoice) => (
+                        <Link key={invoice.id} href={`/dashboard/invoices/${invoice.id}`}>
+                            <li className="bg-cardBg p-4 mb-3 rounded-lg shadow-lg flex justify-between transform transition-transform hover:scale-105 hover:shadow-xl cursor-pointer">
+                                <div className="flex items-center space-x-4">
+                                    <div className="text-xl flex-shrink-0">
+                                        {statusIcon(invoice.status)}
+                                    </div>
+                                    <div className="flex flex-col items-start">
+                                        <span className="block text-sm font-semibold mb-3">Invoice #{invoice.id}</span>
+                                        <span className="block text-sm text-gray-400 mb-3">Client: {invoice.client.name}</span>
+                                        <span className="block text-sm text-gray-400">Amount: ${invoice.totalAmount.toFixed(2)}</span>
+                                    </div>
+                                </div>
+                                <div className="flex items-center space-x-4">
+                                    {statusPill(invoice.status)}
+                                    <Link href={`/invoices/${invoice.id}/download`} className="flex items-center text-gray-400 hover:text-green-500 transition-all duration-300 text-xs">
+                                        <FaFileDownload size={16} className="mr-1" />
+                                    </Link>
+                                </div>
+                            </li>
+                        </Link>
+                    ))}
             </ul>
+            <div className="flex justify-center p-4">
+                {Array(Math.ceil(filteredInvoices.length / invoicesPerPage))
+                    .fill(0)
+                    .map((_, index) => (
+                        <button
+                            key={index}
+                            onClick={() => handlePageChange(index + 1)}
+                            className={`px-3 py-1 mx-1 rounded ${currentPage === index + 1 ? 'bg-blue-600 text-white' : 'bg-gray-800 text-gray-400'}`}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
+            </div>
         </div>
     );
 };
